21be8ce0c62213a03a866ee4d45542c7bd44b1b6
Abdulbasit110 <BASIT4408944@cloud.neduet.edu.pk>
2025-01-23 00:23:15 +0500

    ok

--- server/controllers/donation.controller.js
+++ server/controllers/donation.controller.js
@@ -1,6 +1,6 @@
 import { Donation } from '../models/donation.model.js';  
 import { User } from '../models/user.model.js';          
-import { Ngo } from "../models/ngo.model.js";         
+import { Ngo } from "../models/NGO.model.js";         
 import {ApiError} from '../utils/ApiError.js';             
 import {ApiResponse} from '../utils/ApiResponse.js';       
 
