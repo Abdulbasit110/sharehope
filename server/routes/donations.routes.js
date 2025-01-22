f69030c8a34327edab270948157587d77b6a36e3
Abdulbasit110 <BASIT4408944@cloud.neduet.edu.pk>
2025-01-22 23:35:43 +0500

    resolwed issues

--- server/routes/donations.routes.js
+++ server/routes/donations.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { verifyJWT } from '../middleware/isAuth.middleware.js';
-import { createDonation, getMyDonation } from '../controllers/donation.controller.js';
+import { createDonation} from '../controllers/donation.controller.js';
 
 const router = express.Router();
-router.route('/my-donation').get(verifyJWT,getMyDonation)
-router.route('/my-donation').get(verifyJWT,createDonation)
+// router.route('/my-donation').get(verifyJWT,getMyDonation)
+router.route('/my-donation').post(verifyJWT,createDonation)
 export default router;
